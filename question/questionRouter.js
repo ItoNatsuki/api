8e13131e8514e466c4f7ab5e91e07c83efd85385
ItoNatsuki <mametann72@gmail.com>
2022-06-27 12:47:24 +0900

    coment add

--- question/questionRouter.js
+++ question/questionRouter.js
@@ -6,17 +6,20 @@ const router = express.Router();
 router.post('/',(req,res,next)=>{
     //POSTで送信されたデータをjson形式(オブジェクト)に整形する(expressの内蔵ミドルウェア　express.json())
     questionObj = req.body;
+    //idを生成し、質問データにパラメータとして付与
     const questionId = uuidv4();
     questionObj.id = questionId;
     choicesList = req.body.choice;
+    //選択肢に連番IDと投票数のパラメータを作成
     const choices = [];
     choicesList.forEach((element,index )=> {
         choices.push({id:index,content:element,count:0});
     });
     delete questionObj.choice;
     questionObj.choices = choices;
+    //オブジェクトからJSON文字列に変換
     const questionJson = JSON.stringify(questionObj);
-    const url = 'web_vote_app\\api\\jsons\\'
+    //ファイル書き込み
     try{
         fs.writeFileSync(`C:\\Users\\mamet\\Documents\\my_devs\\nodejs\\web_vote_app\\api\\jsons\\${questionId}.json`,questionJson,'utf8');
         res.send({id:questionId});
--- vote/voteRouter.js
+++ vote/voteRouter.js
@@ -1,32 +1,20 @@
-const { v4: uuidv4 } = require('uuid');
 const  express = require('express');
 const router = express.Router();
-let uuid = uuidv4();
-const testJson = {
-    id:uuid,
-    subject:"注文したい飲み物",
-    choices:[
-        {id:1,content:"ビール",count:0},
-        {id:2,content:"ウーロンハイ",count:0},
-        {id:3,content:"焼酎",count:0},
-        {id:4,content:"お冷",count:0}
-    ],
-    settings:{
-        test:true,
-        test2:true,
-        test3:true,
-        test4:true
-    }
-
-} 
-let jsonList ={"67bc788e-877c-4dfc-af05-3d7eb845315c":testJson}
+const fs = require('fs');
     
 //http://localhost:3000/vote/:idのミドルウェア群
+//GETメソッド(集計状況の確認)
 router.get('/:id',(req,res,next)=>{
-    
+    try{
+        //JSONファイルを開く
+        const questionJsonStr = fs.readFileSync(`C:\\Users\\mamet\\Documents\\my_devs\\nodejs\\web_vote_app\\api\\jsons\\${req.params.id}.json`,'utf8');
+        const questionJson = JSON.parse(questionJsonStr);
+        //httpヘッダーのcontent-typeがjsonで送信される
+        res.json(questionJson);
+    }catch(err){
+        console.log(err);
+    }
 });
-router.get('/:id',(req,res,next)=>{
-    res.send(tempJson);
-})
+
 
 module.exports = router;
