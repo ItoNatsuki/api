4110a647f47e0a4cfdbd3098e4e8fb785ac76dba
ItoNatsuki <mametann72@gmail.com>
2022-06-27 11:34:21 +0900

    questionJson fix questionObj

--- question/questionRouter.js
+++ question/questionRouter.js
@@ -4,16 +4,17 @@ const router = express.Router();
 //http://localhost:3000/questionのミドルウェア群
 router.post('/',(req,res,next)=>{
     //POSTで送信されたデータをjsonに整形する(expressの内蔵ミドルウェア　express.json())
-    questionJson = req.body;
+    questionObj = req.body;
     const questionId = uuidv4();
-    questionJson.id = questionId;
+    questionObj.id = questionId;
     choicesList = req.body.choice;
     const choices = [];
     choicesList.forEach((element,index )=> {
         choices.push({id:index,content:element,count:0});
     });
-    delete questionJson.choice;
-    questionJson.choices = choices;
+    delete questionObj.choice;
+    questionObj.choices = choices;
+    
     res.send({id:questionId});
 })
 module.exports = router;
