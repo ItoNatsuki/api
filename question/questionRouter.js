1b3cf2f3dae860da160491183cbc57133a4be644
ItoNatsuki <mametann72@gmail.com>
2022-06-27 12:03:48 +0900

    jsonfile writing　

--- question/questionRouter.js
+++ question/questionRouter.js
@@ -1,9 +1,10 @@
 const { v4: uuidv4 } = require('uuid');
 const  express = require('express');
+const fs = require('fs');
 const router = express.Router();
 //http://localhost:3000/questionのミドルウェア群
 router.post('/',(req,res,next)=>{
-    //POSTで送信されたデータをjsonに整形する(expressの内蔵ミドルウェア　express.json())
+    //POSTで送信されたデータをjson形式(オブジェクト)に整形する(expressの内蔵ミドルウェア　express.json())
     questionObj = req.body;
     const questionId = uuidv4();
     questionObj.id = questionId;
@@ -14,7 +15,13 @@ router.post('/',(req,res,next)=>{
     });
     delete questionObj.choice;
     questionObj.choices = choices;
-    
-    res.send({id:questionId});
-})
+    const questionJson = JSON.stringify(questionObj);
+    const url = 'web_vote_app\\api\\jsons\\'
+    try{
+        fs.writeFileSync(`C:\\Users\\mamet\\Documents\\my_devs\\nodejs\\web_vote_app\\api\\jsons\\${questionId}.json`,questionJson,'utf8');
+        res.send({id:questionId});
+    }catch(err){
+        console.log(err);
+    }
+});
 module.exports = router;
