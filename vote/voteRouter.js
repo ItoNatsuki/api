e7c4922d3d52d059db634f6930441ef56e1104b1
ItoNatsuki <mametann72@gmail.com>
2022-06-29 19:06:59 +0900

    fileLeader add

--- vote/voteRouter.js
+++ vote/voteRouter.js
@@ -1,16 +1,20 @@
 const { signedCookie } = require('cookie-parser');
 const  express = require('express');
 const router = express.Router();
+const fileLeader = require('../../moduls/fileLeader');
 const fs = require('fs');
-    
+const jsonsLocation = 'api\\jsons'
 //http://localhost:3000/vote/:idのミドルウェア群
 //GETメソッド(集計状況の確認)
 router.get('/:id',(req,res,next)=>{
     try{
+        const questionJson = fileLeader(req.params.id);
         //JSONファイルを開く
+        /*
         const jsonsLocation = 'api\\jsons'
         const questionJsonStr = fs.readFileSync(`${jsonsLocation}\\${req.params.id}.json`,'utf8');
         const questionJson = JSON.parse(questionJsonStr);
+        */
         //httpヘッダーのcontent-typeがjsonで送信される
         res.json(questionJson);
     }catch(err){
@@ -19,9 +23,12 @@ router.get('/:id',(req,res,next)=>{
 });
 router.put('/add/:id',(req,res,next)=>{
     try{
+        const questionJson = fileLeader(req.params.id);
+        /*
         const jsonsLocation = 'api\\jsons'
         const questionJsonStr = fs.readFileSync(`${jsonsLocation}\\${req.params.id}.json`,'utf8');
         const questionJson = JSON.parse(questionJsonStr);
+        */
         questionJson.choices[req.body.id].count++;
         fs.writeFileSync(`${jsonsLocation}\\${req.params.id}.json`,JSON.stringify(questionJson),'utf8');
         console.log(questionJson);
@@ -31,9 +38,12 @@ router.put('/add/:id',(req,res,next)=>{
 });
 router.put('/sub/:id',(req,res,next)=>{
     try{
+        const questionJson = fileLeader(req.params.id);
+        /*
         const jsonsLocation = 'api\\jsons'
         const questionJsonStr = fs.readFileSync(`${jsonsLocation}\\${req.params.id}.json`,'utf8');
         const questionJson = JSON.parse(questionJsonStr);
+        */
         if(questionJson.choices[req.body.id].count>0){
             questionJson.choices[req.body.id].count--;
         }else{
